b1cc25547531266635ed3e3ee4d5dcd2a614354a
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-08-20 16:12:58 +0100

    Generate computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello,World!");
+function getComputerChoice() {
+    const move = Math.floor(Math.random() * 3);
+    if (move === 0) {
+        return 'Rock';
+    } else if (move === 1) {
+        return 'Paper';
+    }
+    return 'Scissors';
+}
+
+console.log(getComputerChoice());
